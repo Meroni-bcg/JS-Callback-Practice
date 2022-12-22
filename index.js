e694b42b51ce05774e239167cfe4329af20045a7
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2022-12-21 19:59:06 -0800

    added event listener

--- index.js
+++ index.js
@@ -13,3 +13,195 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+//keep track of the character's current position
+const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+move(character).to(100, 250)
+
+//distance between the character and the left side of the screen
+let direction = null;
+let x = 100;
+let y = 250;
+
+if(direction === 'west'){
+    x = x - 1
+}
+
+//character's left and bottom properties
+let direction = null;
+let x = 100;
+let y = 250;
+
+if(direction === 'west'){
+    x = x - 1
+}
+if(direction === 'north'){
+    y = y + 1
+}
+if(direction === 'east'){
+    x = x + 1
+}
+if(direction === 'south'){
+    y = y - 1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+
+
+//call the callback repeatedly
+
+
+const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+
+function moveCharacter(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}
+
+//pass that function to setInterval
+
+setInterval(moveCharacter, 1)                                    
+// or we can use an event listener 
+
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+
+//changing direction of arrow keys
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+//stopping the character when user releases the keys
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+// refactor  code into the move function. to  allow us to use it on any image.
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom){
+       
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
+}
+
+const character = newImage('assets/green-character/static.gif')
+move(character).withArrowKeys(100, 250)
+
+//trying it with a new image
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+//change direction callback
+
+function moveWithArrowKeys(left, bottom, callback){
+    let direction = null;
+    let x = left;
+let y = bottom;
+}
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    callback(direction)
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+    callback(direction)
+})
+
